06f8fecf6886b9ed59ebadf422e554ede1d09d59
wipeseals <wipeseals@gmail.com>
2025-03-02 00:22:23 +0900

    feat: enable experimental Nix features in configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -16,7 +16,9 @@
   wsl.enable = true;
   wsl.defaultUser = "nixos";
 
-  # This value determines the NixOS release from which the default
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
