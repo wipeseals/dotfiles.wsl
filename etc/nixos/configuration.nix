bd64145ace8569c099a1cec667f63aa89808ba65
wipeseals <wipeseals@gmail.com>
2025-03-03 00:24:36 +0900

    feat: update configuration to include additional tools and enable starship in fish

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -8,22 +8,45 @@
     };
   };
   environment.systemPackages = with pkgs; [
+    # must have tools
     git
     vim
     curl
     wget
     nix-ld-rs # https://github.com/nix-community/nix-ld
-    fish
+
+    # shell/env tools
     starship
+    fish
+
+    # altenative tools
+    ripgrep # grep replacement
+    bat # cat replacement
+    fd # replacement for find
+    eza # replacement for ls
+
+    # dev tools
+    fzf # fuzzy finder
+    direnv # environment variable manager
+    gh # github cli
+    lazygit # git tui
   ];
 
   programs = {
     nix-ld.enable = true;
+    starship = {
+      enable = true;
+    };
     fish = {
       enable = true;
       interactiveShellInit = ''
         starship init fish | source
+        fzf --fish | source
       '';
+    };
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
     };
   };
   users.users.nixos.shell = pkgs.fish;
--- etc/nixos/home.nix
+++ etc/nixos/home.nix
@@ -5,22 +5,6 @@
     username = "nixos";
     homeDirectory = "/home/nixos";
       packages = with pkgs; [
-        # altenative tools
-        ripgrep # grep replacement
-        bat # cat replacement
-        fd # replacement for find
-        eza # replacement for ls
-
-        # shell/env tools
-        starship # shell prompt
-        tmux
-        byobu
-
-        # dev tools
-        fzf # fuzzy finder
-        direnv # environment variable manager
-        gh # github cli
-        lazygit # git tui
     ];
     stateVersion = "24.11";
 
@@ -35,10 +19,6 @@
       enable = true;
       userName = "wipeseals";
       userEmail = "wipeseals@gmail.com";
-    };
-    direnv = {
-      enable = true;
-      nix-direnv.enable = true;
     };
     gh = {
       enable = true;
