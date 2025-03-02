e1b322d80edfbc18bfa7be367e99ae234110e000
wipeseals <wipeseals@gmail.com>
2025-03-03 00:51:28 +0900

    feat: add garbage collection settings and additional Python packages

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -6,6 +6,11 @@
       auto-optimise-store = true;
       experimental-features = [ "nix-command" "flakes" ];
     };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 6d";
+    };
   };
   environment.systemPackages = with pkgs; [
     # must have tools
@@ -16,20 +21,18 @@
     nix-ld-rs # https://github.com/nix-community/nix-ld
 
     # shell/env tools
-    starship
+    starship # prompt
+    zellij # terminal multiplexer
     fish
-
-    # altenative tools
-    ripgrep # grep replacement
-    bat # cat replacement
-    fd # replacement for find
-    eza # replacement for ls
+    tmux # terminal multiplexer
+    byobu # tmux wrapper
 
     # dev tools
     fzf # fuzzy finder
     direnv # environment variable manager
     gh # github cli
     lazygit # git tui
+    jq # json processor
   ];
 
   programs = {
