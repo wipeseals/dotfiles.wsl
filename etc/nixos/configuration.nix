51a59d2a22edcd1a074b8efac15d324a92d75e3d
wipeseals <wipeseals@gmail.com>
2025-03-02 15:23:31 +0900

    refactor: 改装整理

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -6,10 +6,14 @@
     vim
     curl
     wget
+    fish
     nix-ld-rs # https://github.com/nix-community/nix-ld
   ];
 
   programs = {
     nix-ld.enable = true;
+    fish.enable = true;
   };
+
+  users.defaultUserShell = pkgs.fish;
 }
--- etc/nixos/home.nix
+++ etc/nixos/home.nix
@@ -5,8 +5,6 @@
     username = "nixos";
     homeDirectory = "/home/nixos";
       packages = with pkgs; [
-        # shell
-        fish
         # altenative tools
         ripgrep # grep replacement
         bat # cat replacement
@@ -18,14 +16,10 @@
 
   programs = {
     home-manager.enable = true;
-    fish.enable = true;
     git = {
       enable = true;
       userName = "wipeseals";
       userEmail = "wipeseals@gmail.com";
     };
-  };
-  users.users.nixos = {
-    shell = pkgs.fish;
   };
 }
