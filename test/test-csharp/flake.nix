5d79d4059845a16a3613163c188c5be855457237
wipeseals <wipeseals@gmail.com>
2025-03-03 22:43:11 +0900

    feat: C#プロジェクトの初期設定とflake構成の追加

---
+++ test/test-csharp/flake.nix
@@ -0,0 +1,21 @@
+{
+    description = "A flake for a C# project using dotnet";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = { self, nixpkgs, flake-utils }: 
+        flake-utils.lib.eachDefaultSystem (system:
+        let
+            pkgs = import nixpkgs { inherit system; };
+        in
+        {
+            devShell = pkgs.mkShell {
+                buildInputs = [
+                    pkgs.dotnet-sdk
+                ];
+            };
+        });
+}
